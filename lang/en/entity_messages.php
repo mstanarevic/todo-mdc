710125b897004af1a0c69bb9bcb44ec6e9638846
mstanarevic <mstanarevic@gmail.com>
2022-06-10 00:16:59 +0200

    update localization strings

--- lang/en/entity_messages.php
+++ lang/en/entity_messages.php
@@ -7,4 +7,9 @@ return [
 	'create_error' => 'Unable to create :entity',
 	'update_error' => 'Unable to update :entity',
 	'delete_error' => 'Unable to delete :entity',
+	'view_gate_error' => 'You cannot view this :entity',
+	'update_gate_error' => 'You cannot update this :entity',
+	'delete_gate_error' => 'You cannot delete this :entity',
+	'not_found' => ':entity not found',
+	'index' => ':entity index'
 ];
