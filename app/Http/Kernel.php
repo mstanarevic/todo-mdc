51865b3ab5dad04586c3d67a1a7aa0510112c584
mstanarevic <mstanarevic@gmail.com>
2022-06-10 16:06:02 +0200

    register new middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+	    'to-do-list.owner' => \App\Http\Middleware\EnsureUserIsToDoListOwner::class,
+	    'task-in-to-do-list' => \App\Http\Middleware\EnsureTaskIsInCorrectToDoList::class
     ];
 }
