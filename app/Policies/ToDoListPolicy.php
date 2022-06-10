ac14f1590efe20fd5de03d16c92ac408f7aedaf3
mstanarevic <mstanarevic@gmail.com>
2022-06-10 16:06:26 +0200

    refactor ToDoListPolicy - remove unnecessarry stuff

--- app/Policies/ToDoListPolicy.php
+++ app/Policies/ToDoListPolicy.php
@@ -5,45 +5,19 @@ namespace App\Policies;
 use App\Models\ToDoList;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
-use Illuminate\Auth\Access\Response;
 
 class ToDoListPolicy {
 	use HandlesAuthorization;
 
 	/**
-	 * Determine whether the user can view the model.
+	 * Determine whether the user can manage to do list
 	 *
 	 * @param \App\Models\User $user
 	 * @param \App\Models\ToDoList $toDoList
 	 *
 	 * @return bool
 	 */
-	public function view( User $user, ToDoList $toDoList ) {
+	public function manage( User $user, ToDoList $toDoList ) {
 		return $user->id == $toDoList->user_id;
 	}
-
-	/**
-	 * Determine whether the user can update the model.
-	 *
-	 * @param \App\Models\User $user
-	 * @param \App\Models\ToDoList $toDoList
-	 *
-	 * @return bool
-	 */
-	public function update( User $user, ToDoList $toDoList ) {
-		return $user->id == $toDoList->user_id;
-	}
-
-	/**
-	 * Determine whether the user can delete the model.
-	 *
-	 * @param \App\Models\User $user
-	 * @param \App\Models\ToDoList $toDoList
-	 *
-	 * @return bool
-	 */
-	public function delete( User $user, ToDoList $toDoList ) {
-		return $user->id == $toDoList->user_id;
-	}
-
 }
