c7ebead970fbf915728cd64c3a4674ca7fc32e1e
mstanarevic <mstanarevic@gmail.com>
2022-06-13 12:19:32 +0200

    fix auth route again - prettier

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post( '/login', '\\App\\Http\\Controllers\\Api\\AuthController@login' );
+Route::post( '/auth/login', '\\App\\Http\\Controllers\\Api\\AuthController@login' );
 
 Route::middleware( [ 'auth:sanctum' ] )->group( function () {
 
